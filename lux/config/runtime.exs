8306efc1431cb6d713a366694177fabc0841df1c
Robert Parcus <923630+rparcus@users.noreply.github.com>
2025-02-28 17:15:19 +0900

    ✨ update config and .envrc files

--- lux/config/runtime.exs
+++ lux/config/runtime.exs
@@ -16,9 +16,11 @@ if config_env() in [:dev, :test] do
   config :lux, :api_keys,
     alchemy: env!("ALCHEMY_API_KEY", :string!),
     openai: env!("OPENAI_API_KEY", :string!),
+    anthropic: env!("ANTHROPIC_API_KEY", :string!),
     openweather: env!("OPENWEATHER_API_KEY", :string!),
     transpose: env!("TRANSPOSE_API_KEY", :string!),
     integration_openai: env!("INTEGRATION_OPENAI_API_KEY", :string!, required: false),
+    integration_anthropic: env!("INTEGRATION_ANTHROPIC_API_KEY", :string!, required: false),
     integration_openweather: env!("INTEGRATION_OPENWEATHER_API_KEY", :string!, required: false),
     integration_transpose: env!("INTEGRATION_TRANSPOSE_API_KEY", :string!, required: false)
 
